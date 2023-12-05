545db6777c97662903d60e06f09ce642fa635be0
amir-jafari <amir.h.jafari@okstate.edu>
2023-12-05 13:16:06 -0500

    Added my proposals

--- Report_Gen/script.py
+++ Report_Gen/script.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     input_file_path = 'input.json'
     Year = "2024"
     Semester = "Fall"
-    Version = "2"
+    Version = "3"
 
 
     output_file_path = os.getcwd() + os.sep + f'Arxiv{os.sep}Proposals{os.sep}{Year}{os.sep}{Semester}{os.sep}{Version}{os.sep}'
