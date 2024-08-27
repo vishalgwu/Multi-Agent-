3f73c36f6ddf03f61c40cb4c274eb0f5089bc501
Amir <amir.h.jafari@okstate.edu>
2024-08-27 10:31:49 -0400

    Adjust expected student team size.
    Changed the expected number of students from 3-4 to 1-2 for the project scope. This modification reflects a reassessment of the workload and dataset complexity.

--- Report_Gen/Arxiv/Proposals/2024/Fall/3/json_gen.py
+++ Report_Gen/Arxiv/Proposals/2024/Fall/3/json_gen.py
@@ -98,7 +98,7 @@ data_to_save = \
         "Expected Number Students":
             """
             Given the scope of work and the need to explore multiple datasets and modeling techniques, this project is suitable 
-            for a team of 3-4 students.
+            for a team of 1-2 students.
             """,
         # -----------------------------------------------------------------------------------------------------------------------
         "Possible Issues":
